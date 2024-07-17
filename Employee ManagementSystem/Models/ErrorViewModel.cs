2fc1eb5c3726fa6d3d549c648aec229dfc82d8f3
TundeOyinloye <sogsoggodsson@gmail.com>
2024-07-17 19:12:52 +0100

    Add project files.

---
+++ Employee ManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Employee_ManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
